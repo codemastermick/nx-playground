920e2b26900d37634241f3761bb9e4fda267bfa9
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-03-18 21:20:40 -0400

    chore(todos): export additional items from module

--- libs/todos/src/index.ts
+++ libs/todos/src/index.ts
@@ -1,1 +1,6 @@
 export * from './lib/todos.module';
+export * from './lib/todos.service';
+
+export * from './lib/dto/create-todo.dto';
+export * from './lib/dto/create-todo.dto';
+export * from './lib/entities/todo.entity';

54f219b2cb1cbcc44ce49e14a938226594eb4b2b
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-03-19 16:38:20 -0400

    docs(ui): improve storybook docs for todo

--- libs/ui/src/lib/todo/todo.component.stories.ts
+++ libs/ui/src/lib/todo/todo.component.stories.ts
@@ -9,19 +9,28 @@ export default {
     actions: {
       handles: ['mouseover', 'change', 'click', 'focusout'],
     },
+    docs: {
+      description: {
+        component: 'The Angular component for displaying a Todo item',
+      },
+    },
   },
   argTypes: {
     todo: {
       name: 'todo',
       description: 'The todo item being displayed by this component',
-      defaultValue: undefined,
+      defaultValue: {
+        summary: typeof undefined,
+      },
     },
     editingContents: {
       name: 'editingContents',
       description:
         'An internal boolean value used by the component to determine the visibility of the input field',
       type: 'boolean',
-      defaultValue: false,
+      defaultValue: {
+        summary: false,
+      },
     },
   },
   decorators: [
@@ -36,22 +45,53 @@ const Template: Story<TodoComponent> = (args: TodoComponent) => ({
 });
 
 export const NoData = Template.bind({});
+NoData.parameters = {
+  docs: {
+    description: {
+      story: 'When todo data is **not** supplied',
+    },
+  },
+};
 NoData.args = {
   todo: undefined,
   editingContents: false,
 };
+
 export const WithData = Template.bind({});
 WithData.args = {
   todo: { id: '-1', contents: 'Incomplete Item', done: false },
   editingContents: false,
 };
+WithData.parameters = {
+  docs: {
+    description: {
+      story: 'When todo data **is** supplied',
+    },
+  },
+};
+
 export const IsDone = Template.bind({});
 IsDone.args = {
   todo: { id: '-1', contents: 'Completed Item', done: true },
   editingContents: false,
 };
+IsDone.parameters = {
+  docs: {
+    description: {
+      story: 'When the todo is marked completed',
+    },
+  },
+};
+
 export const Editing = Template.bind({});
 Editing.args = {
   todo: { id: '-1', contents: 'Editing Item', done: false },
   editingContents: true,
+};
+Editing.parameters = {
+  docs: {
+    description: {
+      story: 'When the todo contents are being edited',
+    },
+  },
 };

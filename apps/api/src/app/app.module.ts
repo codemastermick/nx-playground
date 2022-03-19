09bed2b1c9095a59c5b626ec1f358a7fc402edaa
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-03-18 21:10:34 -0400

    feat(api): import todos module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TodosModule } from '@playground/todos';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [TodosModule],
   controllers: [AppController],
   providers: [AppService],
 })
